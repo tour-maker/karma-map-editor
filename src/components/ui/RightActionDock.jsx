1f1527f9851399f2b0f219ac2cbd45ac8cf5fb34
aryan-tolanii <tolaniaryan10@gmail.com>
2026-08-25 10:12:45 +0530

    Change share icon to RiShareBoxLine for curved arrow inside square box

--- src/components/ui/RightActionDock.jsx
+++ src/components/ui/RightActionDock.jsx
@@ -1,4 +1,4 @@
-import { FiExternalLink } from 'react-icons/fi';
+import { RiShareBoxLine } from 'react-icons/ri';
 import React, { useState, useEffect } from 'react';
 import { useMapStore } from '../../store/useMapStore';
 import { FiShare2, FiSliders, FiX, FiHelpCircle, FiVolume2, FiVolumeX, FiCamera, FiMaximize, FiMinimize } from 'react-icons/fi';
@@ -266,7 +266,7 @@ https://karma-map-editor.onrender.com/`;
         onClick={handleShare}
         title="Share map view"
       >
-        <FiExternalLink size={26} />
+        <RiShareBoxLine size={26} />
       </button>
 
       {/* Outer Dock Container Anchored at Top 38% (NO transform: translateY(-50%) so top buttons NEVER move when opening!) */}
